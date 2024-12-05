cf39cdf7ade3ad50a05e521bd95f1a49f477971a
takashimomose <taka.yao100@gmail.com>
2024-12-05 13:26:04 +0900

    一般ユーザー：勤怠登録画面_出勤後のBlade作成

--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -7,4 +7,19 @@
 @endpush
 
 @section('content')
+    <main class="wrapper">
+        <section class="attendance-section">
+            <div class="container">
+                <div class="status">勤務外</div>
+                <div class="date">
+                    {{ \Carbon\Carbon::now()->isoFormat('Y年M月D日') .
+                        '(' .
+                        ['日', '月', '火', '水', '木', '金', '土'][\Carbon\Carbon::now()->dayOfWeek] .
+                        ')' }}
+                </div>
+                <div class="time">{{ \Carbon\Carbon::now()->format('H:i') }}</div>
+                <button type="submit" class="start-btn">出勤</button>
+            </div>
+        </section>
+    </main>
 @endsection
