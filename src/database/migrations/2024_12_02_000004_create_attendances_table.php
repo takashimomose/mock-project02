553660aeaa9a52fa66235d5c71fa2d7791224f4f
takashimomose <taka.yao100@gmail.com>
2024-12-05 22:35:16 +0900

    一般ユーザー：attendancesテーブルのマイグレーションファイル名修正とattendance_statusテーブルのマイグレーションファイル作成

---
+++ src/database/migrations/2024_12_02_000004_create_attendances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time')->nullable();
+            $table->timestamps();
+
+            // 外部キーの設定
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
