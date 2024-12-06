69a3b8537d288884607c300c49ec39994cb59431
takashimomose <taka.yao100@gmail.com>
2024-12-06 11:47:33 +0900

    一覧ユーザー：勤怠登録画面の不要なdiv削除

--- src/resources/views/attendance.blade.php
+++ src/resources/views/attendance.blade.php
@@ -11,36 +11,34 @@
         <section class="attendance-section">
             <form action="{{ route('attendance.store') }}" method="POST">
                 @csrf
-                <div class="container">
-                    <div class="status">
-                        @if ($workingStatus === 1)
-                            勤務外
-                        @elseif ($workingStatus === 2)
-                            出勤中
-                        @elseif ($workingStatus === 3)
-                            休憩中
-                        @elseif ($workingStatus === 4)
-                            退勤済
-                        @endif
-                    </div>
-                    <div class="date">
-                        {{ \Carbon\Carbon::now()->isoFormat('Y年M月D日') .
-                            '(' .
-                            ['日', '月', '火', '水', '木', '金', '土'][\Carbon\Carbon::now()->dayOfWeek] .
-                            ')' }}
-                    </div>
-                    <div class="time">{{ \Carbon\Carbon::now()->format('H:i') }}</div>
+                <div class="status">
                     @if ($workingStatus === 1)
-                        <button type="submit" class="start-btn" name="start_work" value="2">出勤</button>
+                        勤務外
                     @elseif ($workingStatus === 2)
-                        <button type="submit" class="end-btn" name="end_work" value="4">退勤</button>
-                        <button type="submit" class="start-break-btn" name="start_break" value="3">休憩入</button>
+                        出勤中
                     @elseif ($workingStatus === 3)
-                        <button type="submit" class="end-break-btn" name="end_break" value="2">休憩戻</button>
+                        休憩中
                     @elseif ($workingStatus === 4)
-                        <p class="message">お疲れ様でした。</p>
+                        退勤済
                     @endif
                 </div>
+                <div class="date">
+                    {{ \Carbon\Carbon::now()->isoFormat('Y年M月D日') .
+                        '(' .
+                        ['日', '月', '火', '水', '木', '金', '土'][\Carbon\Carbon::now()->dayOfWeek] .
+                        ')' }}
+                </div>
+                <div class="time">{{ \Carbon\Carbon::now()->format('H:i') }}</div>
+                @if ($workingStatus === 1)
+                    <button type="submit" class="start-btn" name="start_work" value="2">出勤</button>
+                @elseif ($workingStatus === 2)
+                    <button type="submit" class="end-btn" name="end_work" value="4">退勤</button>
+                    <button type="submit" class="start-break-btn" name="start_break" value="3">休憩入</button>
+                @elseif ($workingStatus === 3)
+                    <button type="submit" class="end-break-btn" name="end_break" value="2">休憩戻</button>
+                @elseif ($workingStatus === 4)
+                    <p class="message">お疲れ様でした。</p>
+                @endif
             </form>
         </section>
     </main>
