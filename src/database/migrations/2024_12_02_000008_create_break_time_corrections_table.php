698ff144d63077e26e16e3cd52b541d62e7497bb
takashimomose <taka.yao100@gmail.com>
2024-12-11 16:02:47 +0900

    一般ユーザー：勤怠詳細画面、break_time_correctionsテーブルのマイグレーションファイル修正

--- src/database/migrations/2024_12_02_000008_create_break_time_corrections_table.php
+++ src/database/migrations/2024_12_02_000008_create_break_time_corrections_table.php
@@ -15,13 +15,13 @@ class CreateBreakTimeCorrectionsTable extends Migration
     {
         Schema::create('break_time_corrections', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('attendance_correction_id');
+            $table->unsignedBigInteger('attendance_id');
             $table->timestamp('start_time')->nullable();
             $table->timestamp('end_time')->nullable();
             $table->timestamps();
 
             // 外部キーの設定
-            $table->foreign('attendance_correction_id')->references('id')->on('attendance_corrections')->onDelete('cascade');
+            $table->foreign('attendance_id')->references('id')->on('attendances')->onDelete('cascade');
         });
     }
 
