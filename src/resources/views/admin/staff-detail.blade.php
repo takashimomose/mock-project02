f78d4430f8c6e7892cf440f8b0813fadbcedfea9
takashimomose <taka.yao100@gmail.com>
2025-01-07 11:37:14 +0900

    管理者：ユーザー別かつ月別で勤怠をCSVで出力する機能の実装

--- src/resources/views/admin/staff-detail.blade.php
+++ src/resources/views/admin/staff-detail.blade.php
@@ -11,12 +11,14 @@
         <section class="attendance-list-section">
             <h1>{{ $user->name }}さんの勤怠</h1>
             <div class="attendance-header">
-                <a href="{{ url('/admin/attendance/staff/' . $user->id . '?month=' . $previousMonth) }}" class="prev-btn">&larr; 前月</a>
+                <a href="{{ url('/admin/attendance/staff/' . $user->id . '?month=' . $previousMonth) }}"
+                    class="prev-btn">&larr; 前月</a>
                 <span class="current-month">
                     <img src="{{ asset('images/calendar.svg') }}" class="calendar-icon" alt="calendar-icon">
                     {{ $currentMonth->format('Y/n') }}
                 </span>
-                <a href="{{ url('/admin/attendance/staff/' . $user->id . '?month=' . $nextMonth) }}" class="next-btn">翌月 &rarr;</a>
+                <a href="{{ url('/admin/attendance/staff/' . $user->id . '?month=' . $nextMonth) }}" class="next-btn">翌月
+                    &rarr;</a>
             </div>
             <table class="attendance-table">
                 <thead>
@@ -35,7 +37,8 @@
                             <td>{{ \Carbon\Carbon::parse($attendance->date)->isoFormat('M/D') .
                                 '(' .
                                 ['日', '月', '火', '水', '木', '金', '土'][\Carbon\Carbon::parse($attendance->date)->dayOfWeek] .
-                                ')' }}</td>
+                                ')' }}
+                            </td>
                             <td>{{ $attendance->start_time }}</td>
                             <td>{{ $attendance->end_time }}</td>
                             <td>{{ optional($breakTimes->firstWhere('attendance_id', $attendance->id))->formatted_break_time ?? '-' }}
@@ -48,7 +51,10 @@
                     @endforeach
                 </tbody>
             </table>
-            <button class="csv-btn" type="submit">CSV出力</button>
+            <form method="GET" action="{{ route('admin.staff.export', ['id' => $user->id]) }}">
+                <input type="hidden" name="month" value="{{ $currentMonth->format('Y-m') }}">
+                <button class="csv-btn" type="submit">CSV出力</button>
+            </form>
         </section>
     </main>
 @endsection
