3a3e20f998b92acd25c5f2e27de7d1da62c2ddbe
takashimomose <taka.yao100@gmail.com>
2025-01-17 10:07:50 +0900

    PHPUnitテスト実装：勤怠詳細情報修正機能（一般ユーザー）途中2

--- src/tests/Feature/AttendanceCorrectionTest.php
+++ src/tests/Feature/AttendanceCorrectionTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Attendance;
+use App\Models\AttendanceCorrection;
 use App\Models\BreakTime;
 use App\Models\User;
 use Illuminate\Support\Carbon;
@@ -110,11 +111,208 @@ class AttendanceCorrectionTest extends TestCase
 
         // 休憩時間を退勤時間より後に入力
         $response = $this->post('/attendance/correct/general', [
-            'break_start_time' => '19:00',
-            // 'break_end_time' => '20:00',
+            'start_time' => '09:00',
+            'end_time' => '18:00',
+            'break_start_time' => ['0' => '19:00'],
         ]);
 
         // バリデーションメッセージを確認
-        $response->assertSessionHasErrors(['break_time_before_end_time' => '休憩時間が不適切な値です']);
+        $response->assertSessionHasErrors(['break_time_out_of_range.0' => '休憩時間が勤務時間外です']);
+    }
+
+    public function test_break_end_time()
+    {
+        // ログインのユーザーを作成
+        $user = User::create([
+            'role_id' => User::ROLE_GENERAL,
+            'name' => 'テストユーザー',
+            'email' => 'registered@example.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
+
+        // ログインページへのアクセス
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        // ログインを試行
+        $response = $this->post('/login', [
+            'email' => 'registered@example.com',
+            'password' => 'password123',
+        ]);
+
+        // 認証されていることを確認
+        $this->assertAuthenticatedAs($user);
+
+        // 勤怠画面へのリダイレクトを確認
+        $response->assertRedirect('/attendance');
+
+        // 勤怠データを作成
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'date' => Carbon::now()->startOfMonth()->toDateString(),
+            'start_time' => '09:00:00',
+            'end_time' => '18:00:00',
+            'working_hours' => 480,
+            'attendance_status_id' => Attendance::STATUS_FINISHED,
+        ]);
+
+        $response = $this->get('/attendance/list');
+        $response->assertSee('詳細');
+
+        $response = $this->get('/attendance/' . $attendance->id);
+
+        // 休憩時間を退勤時間より後に入力
+        $response = $this->post('/attendance/correct/general', [
+            'start_time' => '09:00',
+            'end_time' => '18:00',
+            'break_start_time' => ['0' => '19:00'],
+            'break_end_time' => ['0' => '20:00'],
+        ]);
+
+        // バリデーションメッセージを確認
+        $response->assertSessionHasErrors(['break_within_working_hours.0' => '休憩時間が勤務時間外です']);
+    }
+
+    public function test_reason()
+    {
+        // ログインのユーザーを作成
+        $user = User::create([
+            'role_id' => User::ROLE_GENERAL,
+            'name' => 'テストユーザー',
+            'email' => 'registered@example.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
+
+        // ログインページへのアクセス
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        // ログインを試行
+        $response = $this->post('/login', [
+            'email' => 'registered@example.com',
+            'password' => 'password123',
+        ]);
+
+        // 認証されていることを確認
+        $this->assertAuthenticatedAs($user);
+
+        // 勤怠画面へのリダイレクトを確認
+        $response->assertRedirect('/attendance');
+
+        // 勤怠データを作成
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'date' => Carbon::now()->startOfMonth()->toDateString(),
+            'start_time' => '09:00:00',
+            'end_time' => '18:00:00',
+            'working_hours' => 480,
+            'attendance_status_id' => Attendance::STATUS_FINISHED,
+        ]);
+
+        $response = $this->get('/attendance/list');
+        $response->assertSee('詳細');
+
+        $response = $this->get('/attendance/' . $attendance->id);
+
+        // 休憩時間を退勤時間より後に入力
+        $response = $this->post('/attendance/correct/general', [
+            'reason' => null,
+        ]);
+
+        // バリデーションメッセージを確認
+        $response->assertSessionHasErrors(['reason' => '備考を入力してください']);
+    }
+
+    public function test_attendance_correction_request()
+    {
+        // ログインのユーザーを作成
+        $user = User::create([
+            'role_id' => User::ROLE_GENERAL,
+            'name' => 'テストユーザー',
+            'email' => 'registered@example.com',
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
+        ]);
+
+        // ログインページへのアクセス
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+
+        // ログインを試行
+        $response = $this->post('/login', [
+            'email' => 'registered@example.com',
+            'password' => 'password123',
+        ]);
+
+        // 認証されていることを確認
+        $this->assertAuthenticatedAs($user);
+
+        // 勤怠画面へのリダイレクトを確認
+        $response->assertRedirect('/attendance');
+
+        // 勤怠データを作成
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'date' => Carbon::now()->startOfMonth()->toDateString(),
+            'start_time' => '09:00:00',
+            'end_time' => '18:00:00',
+            'working_hours' => 480,
+            'attendance_status_id' => Attendance::STATUS_FINISHED,
+        ]);
+
+        $response = $this->get('/attendance/list');
+        $response->assertSee('詳細');
+
+        $response = $this->get('/attendance/' . $attendance->id);
+
+        $response = $this->post('/attendance/correct/general', [
+            'date_year' => Carbon::now()->format('Y年'),
+            'date_day' => Carbon::now()->format('m月d日'),
+            'start_time' => '08:00',
+            'end_time' => '17:00',
+            'attendance_id' => $attendance->id,
+            'request_date' => Carbon::now(),
+            'correction_status_id' => AttendanceCorrection::PENDING,
+            'reason' => '勤怠を間違えたため修正お願いします。'
+        ]);
+
+        $response->assertRedirect('/attendance/list');
+
+        // ログアウト
+        $this->post('/logout');
+
+        // 管理者ユーザーを作成
+        $adminUser = User::create([
+            'role_id' => User::ROLE_ADMIN,
+            'name' => '管理者ユーザー',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('adminpassword123'),
+        ]);
+
+        // 管理者ログインページにアクセス
+        $response = $this->get('/admin/login');
+        $response->assertStatus(200);
+
+        // 管理者ログインを試行
+        $response = $this->post('/admin/login', [
+            'email' => 'admin@example.com',
+            'password' => 'adminpassword123',
+        ]);
+
+        // 認証されていることを確認
+        $this->assertAuthenticatedAs($adminUser);
+
+        // 申請一覧画面にアクセス
+        $response = $this->get('/stamp_correction_request/list');
+        $response->assertStatus(200);
+
+        $response->assertSee('承認待ち');
+        $response->assertSee($user->name);
+        $response->assertSee($user->date);
+        $response->assertSee('勤怠を間違えたため修正お願いします。');
+        $response->assertSee(Carbon::now()->Format('n月j日'));
+        $response->assertSee('詳細');
     }
 }
